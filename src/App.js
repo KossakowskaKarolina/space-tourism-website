efe82f0f1df84ac875706dc29d51257d1ae80455
Karolina Kossakowska <kossakowska.karolina1@gmail.com>
2022-07-30 22:19:42 +0200

    Create Routes for views components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import Home from './components/views/Home';
+import Destination from './components/views/Destination';
+import Crew from './components/views/Crew';
+import Technology from './components/views/Technology';
+import NotFound from './components/views/NotFound';
 
 const App = () => {
   return (
-    <div>App</div>
+    <Routes>
+      <Route exact path='/' element={<Home />} />
+      <Route exact path='/destination' element={<Destination />} />
+      <Route exact path='/crew' element={<Crew/>} />
+      <Route exact path='/technology' element={<Technology />}/>
+      <Route path='*' element={<NotFound />}/>
+    </Routes>
   )
 }
 
