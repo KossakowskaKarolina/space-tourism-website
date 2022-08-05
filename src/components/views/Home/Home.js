6fd0346af9284eb1912ddde94e23bf75267c59d3
Karolina Kossakowska <kossakowska.karolina1@gmail.com>
2022-08-05 15:55:20 +0200

    Style Home component

---
+++ src/components/views/Home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from './Home.module.css';
+
+import MainLayout from "../../layout/MainLayout";
+
+const Home = () => (
+    <div>
+      <MainLayout />
+    </div>
+  )
+
+
+export default Home;
