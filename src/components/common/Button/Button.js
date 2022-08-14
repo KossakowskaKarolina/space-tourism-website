00ffa70df470453385875beafd55e5a429aeeb8d
Karolina Kossakowska <kossakowska.karolina1@gmail.com>
2022-08-14 16:53:37 +0200

    Fix border in Button

--- src/components/common/Button/Button.js
+++ src/components/common/Button/Button.js
@@ -1,13 +1,10 @@
 import React from "react";
 import styles from './Button.module.css'
 
-const Button = props => {
-
-
-  return (
+const Button = props => (
   <button>{props.text}</button>
 )
-}
+
 
 
 export default Button;
