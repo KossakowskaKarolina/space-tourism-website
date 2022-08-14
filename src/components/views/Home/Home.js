23a07a896477bdd9704f97c546327b51db6b453e
Karolina Kossakowska <kossakowska.karolina1@gmail.com>
2022-08-14 18:25:23 +0200

    Add Hero component to Home view

--- src/components/views/Home/Home.js
+++ src/components/views/Home/Home.js
@@ -2,10 +2,12 @@ import React from "react";
 import styles from './Home.module.css';
 
 import MainLayout from "../../layout/MainLayout";
+import Hero from "../../layout/Hero/Hero";
 
 const Home = () => (
     <div className={styles.body}>
       <MainLayout />
+      <Hero />
     </div>
   )
 
