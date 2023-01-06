4d7f551ca29cee1b56d334bc9be6d730c800daee
Karolina Kossakowska <kossakowska.karolina1@gmail.com>
2023-01-06 13:18:27 +0100

    Minor changes in Destination component

--- src/components/views/Destination/Destination.js
+++ src/components/views/Destination/Destination.js
@@ -2,13 +2,18 @@ import React from "react";
 import styles from './Destination.module.css';
 
 import MainLayout from "../../layout/MainLayout";
+import TitleText from "../../common/TitleText/TitleText";
+import DestinationBox from "../../layout/DestinationBox/DestinationBox";
 
-const Destination = props => {
-  return(
+const Destination = () => {
+  return (
     <div className={styles.body}>
       <MainLayout />
+      <TitleText text='01' otherText='Pick your destination' />
+      <DestinationBox />
     </div>
   )
+
 }
 
 export default Destination;
