6a0112acf040793256ba2b2a289bd7e767a5f578
Karolina <kossakowska.karolina1@gmail.com>
2023-01-16 21:23:15 +0100

    Refactor rendering tabs in Destination component to dynamic rendering

--- src/components/views/Destination/Destination.js
+++ src/components/views/Destination/Destination.js
@@ -10,13 +10,14 @@ import DestinationBox from "../../layout/DestinationBox/DestinationBox";
 import data from '../../../data.json';
 
 const Destination = () => {
-  const [value, setValue] = useState('1');
+  const [value, setValue] = useState('Moon');
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
   let [moon, mars, europa, titan] = data.destinations;
+  let newData = data.destinations;
 
   return (
     <div className={styles.body}>
@@ -24,47 +25,23 @@ const Destination = () => {
       <TitleText text='01' otherText='Pick your destination' />
       <TabContext value={value}>
         <TabList onChange={handleChange}>
-          <Tab label={moon.name} value="1" />
-          <Tab label={mars.name} value="2" />
-          <Tab label={europa.name} value="3" />
-          <Tab label={titan.name} value="4" />
+          <Tab label={moon.name} value="Moon" />
+          <Tab label={mars.name} value="Mars" />
+          <Tab label={europa.name} value="Europa" />
+          <Tab label={titan.name} value="Titan" />
         </TabList>
-        <TabPanel value="1">
+
+        {newData.map(item => 
+          <TabPanel key={item.name} value={item.name}>
           <DestinationBox 
-            image={moon.images.png}
-            name={moon.name}
-            description={moon.description}
-            distance={moon.distance}
-            travel={moon.travel}
+            image={item.images.png}
+            name={item.name}
+            description={item.description}
+            distance={item.distance}
+            travel={item.travel}
           />
-        </TabPanel>
-        <TabPanel value="2">
-          <DestinationBox 
-              image={mars.images.png}
-              name={mars.name}
-              description={mars.description}
-              distance={mars.distance}
-              travel={mars.travel}
-            />
-        </TabPanel>
-        <TabPanel value="3">
-        < DestinationBox 
-             image={europa.images.png}
-             name={europa.name}
-             description={europa.description}
-             distance={europa.distance}
-             travel={europa.travel}
-           />
-        </TabPanel>
-        <TabPanel value="4">
-          <DestinationBox 
-              image={titan.images.png}
-              name={titan.name}
-              description={titan.description}
-              distance={titan.distance}
-              travel={titan.travel}
-            />
-        </TabPanel>
+          </TabPanel>
+       )}
       </TabContext> 
     </div>
   )
