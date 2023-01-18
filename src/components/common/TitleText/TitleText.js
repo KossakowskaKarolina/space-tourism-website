b1bf5d4e5c38d2d0aee0c5c0ab1109dd9fb51b2f
Karolina <kossakowska.karolina1@gmail.com>
2023-01-18 10:38:32 +0100

    Update TitleText styling

--- src/components/common/TitleText/TitleText.js
+++ src/components/common/TitleText/TitleText.js
@@ -2,13 +2,14 @@ import React from "react";
 import PropTypes from 'prop-types'
 import styles from './TitleText.module.css'
 
-const TitleText = ({text, otherText}) => (
-  <h5><span className={styles.text}>{text}</span>{otherText}</h5>
+const TitleText = ({text, otherText, className}) => (
+  <h5 className={className}><span className={styles.text}>{text}</span>{otherText}</h5>
 )
 
 TitleText.propTypes = {
   text: PropTypes.string,
   otherText: PropTypes.string,
+  className: PropTypes.string,
 }
 
 export default TitleText;
