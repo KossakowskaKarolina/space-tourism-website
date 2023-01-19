fd1189094b1789242463c7f35ecd7bb3d102baf6
Karolina <kossakowska.karolina1@gmail.com>
2023-01-19 20:24:53 +0100

    Refactor props notation for TitleText

--- src/components/common/TitleText/TitleText.js
+++ src/components/common/TitleText/TitleText.js
@@ -2,8 +2,8 @@ import React from "react";
 import PropTypes from 'prop-types'
 import styles from './TitleText.module.css'
 
-const TitleText = ({text, otherText, className}) => (
-  <h5 className={className}><span className={styles.text}>{text}</span>{otherText}</h5>
+const TitleText = (props) => (
+  <h5 className={props.className}><span className={styles.text}>{props.text}</span>{props.otherText}</h5>
 )
 
 TitleText.propTypes = {
