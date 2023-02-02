a8a6c483946635e45712a7ca2f3e1a7e78995882
Karolina <kossakowska.karolina1@gmail.com>
2023-02-02 20:05:00 +0100

    Add className props to BodyText component

--- src/components/common/BodyText/BodyText.js
+++ src/components/common/BodyText/BodyText.js
@@ -2,12 +2,13 @@ import React from "react";
 import PropTypes from 'prop-types'
 import styles from './BodyText.module.css'
 
-const BodyText = ({text}) => (
-  <p>{text}</p>
+const BodyText = ({className, text}) => (
+  <p className={className}>{text}</p>
 )
 
 BodyText.propTypes = {
   text: PropTypes.string,
+  className: PropTypes.string,
 }
 
 export default BodyText;
