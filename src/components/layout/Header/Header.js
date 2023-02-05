62074932e06a50476baf723fcda9c6fa1ffe3bcb
Karolina <kossakowska.karolina1@gmail.com>
2023-02-05 22:30:42 +0100

    Add hover effect on navigation

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -1,31 +1,63 @@
 import React from "react";
 import { Grid, Box } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 import styles from './Header.module.css';
 import { ReactComponent as Logo } from '../../../assets/shared/logo.svg';
 // import { ReactComponent as Hamburger } from '../../assets/shared/icon-hamburger.svg'
 
-const Header = () => (
-  <Grid container>
-    <Grid item sm={1} xs={2}>
-      <Logo className={styles.logo}/>
-    </Grid>
-    <Grid item sm={4} xs={8}>
-      <div className={styles.line}></div>
-    </Grid>
-    <Grid item sm={7} xs={2}>
-    {/*<Hamburger /> figure out hamburger menu*/}
-      <Box className={styles.navbar}>
-        <Box className={styles.links}>
-          <Link to='/'><span className={styles.numbers}>00</span>Home</Link>
-          <Link to='/destination'><span className={styles.numbers}>01</span>Destination</Link>
-          <Link to='/crew'><span className={styles.numbers}>02</span>Crew</Link>
-          <Link to='/technology'><span className={styles.numbers}>03</span>Technology</Link>
+const Header = () => {
+
+  let activeClassName = styles.active;
+
+  return (
+    <Grid container>
+      <Grid item sm={1} xs={2}>
+        <Logo className={styles.logo}/>
+      </Grid>
+      <Grid item sm={4} xs={8}>
+        <div className={styles.line}></div>
+      </Grid>
+      <Grid item sm={7} xs={2}>
+      {/*<Hamburger /> figure out hamburger menu*/}
+        <Box className={styles.navbar}>
+          <Box className={styles.links}>
+            <NavLink 
+              to='/'
+              className={({ isActive }) =>
+                isActive ? activeClassName : undefined
+              }>
+              <span className={styles.numbers}>00</span>
+              Home
+            </NavLink>
+            <NavLink 
+              to='/destination'
+              className={({ isActive }) =>
+                isActive ? activeClassName : undefined
+              }>
+              <span className={styles.numbers}>01</span>
+              Destination
+            </NavLink>
+            <NavLink 
+              to='/crew'
+              className={({ isActive }) =>
+                isActive ? activeClassName : undefined
+              }>
+              <span className={styles.numbers}>02</span>
+              Crew
+            </NavLink>
+            <NavLink 
+              to='/technology'
+              className={({ isActive }) =>
+                isActive ? activeClassName : undefined
+              }>
+              <span className={styles.numbers}>03</span>
+              Technology
+            </NavLink>
+          </Box>
         </Box>
-      </Box>
+      </Grid>
     </Grid>
-  </Grid>
-)
+)}
 
 export default Header;
